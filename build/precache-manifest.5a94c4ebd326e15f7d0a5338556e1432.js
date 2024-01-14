d927b48dae9949f1fc31afce4a587677fe85e45c
pavan-2562003 <19275pavankumar@gmail.com>
2024-01-14 17:12:37 +0530

    first commit

---
+++ build/precache-manifest.5a94c4ebd326e15f7d0a5338556e1432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a902aa161061759ce389a57efdd3c0fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a990232b6ec54ce19e03",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d47bc2bfb9ded52a18f3",
+    "url": "/static/css/main.800132b6.chunk.css"
+  },
+  {
+    "revision": "a990232b6ec54ce19e03",
+    "url": "/static/js/2.9eb6aa01.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9eb6aa01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d47bc2bfb9ded52a18f3",
+    "url": "/static/js/main.a5119dd8.chunk.js"
+  },
+  {
+    "revision": "e1fcef772f1fdaabae77",
+    "url": "/static/js/runtime-main.466e3532.js"
+  }
+]);
